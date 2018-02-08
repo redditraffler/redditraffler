0c85ecab5a724cffa64ee12c785ab114a8732f52
Timothy Ng <timnjj@gmail.com>
2018-02-07 19:01:41 -0800

    Remove existing web and api folders

--- api/app.py
+++
@@ -1,7 +0,0 @@
-import falcon
-from api.controllers import raffles
-
-api = application = falcon.API()
-
-api.add_route('/raffles', raffles.Collection())
-api.add_route('/raffles/{submission_id}', raffles.Item())
