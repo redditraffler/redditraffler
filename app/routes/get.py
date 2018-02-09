44526e55af0995fbab7bc9adc3ec45fc216f4f6d
Timothy Ng <timnjj@gmail.com>
2018-02-09 09:42:23 -0800

    Added simple reddit auth flow in backend

--- app/routes/get.py
+++ app/routes/get.py
@@ -1,8 +1,40 @@
-from flask import Blueprint, render_template
+from flask import (
+    abort,
+    Blueprint,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for
+)
+from app.lib import reddit
 
 get = Blueprint('get', __name__)
 
 
 @get.route('/')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template(
+        'index.html',
+        title='Home',
+        reddit_login_url=reddit.get_auth_url()
+    )
+
+
+@get.route('/auth_redirect')
+def auth_redirect():
+    if request.args.get('error') == 'access_denied':
+        # User clicked deny access
+        abort(500)
+    elif request.args.get('state') != 'authorized':
+        abort(500)
+
+    refresh_token = reddit.authorize(request.args.get('code'))
+    session['reddit_refresh_token'] = refresh_token
+    session['reddit_username'] = reddit.get_username(refresh_token)
+    return redirect(url_for('get.raffle_form'))
+
+
+@get.route('/raffles/create')
+def raffle_form():
+    return render_template('raffle_form.html')
