0265b2f7ae667ef332f38198128aadcd5836dfe7
Timothy Ng <timnjj@gmail.com>
2018-02-14 11:32:27 -0800

    Added JSON api endpoint to return user submissions

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,15 @@
-from flask import Blueprint, session, jsonify
+from flask import Blueprint, session, jsonify, abort
 from app.util import reddit
 
 api = Blueprint('api', __name__)
 
 
+@api.route('/submissions')
+def submissions():
+    """ Return the user's Reddit submissions. """
+
+    if 'reddit_refresh_token' not in session:
+        abort(401)
+
+    submissions = reddit.get_user_submissions(session['reddit_refresh_token'])
+    return jsonify(submissions)
