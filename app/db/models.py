cd9497e978f2ce2212ba00157bc60d75af5803fb
Timothy Ng <timnjj@gmail.com>
2018-02-21 18:09:45 -0800

    Added user model

---
+++ app/db/models.py
@@ -0,0 +1,12 @@
+from app.db import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
