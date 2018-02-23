bdddd08cb56f00d7daee8e05c8af9204e446103a
Timothy Ng <timnjj@gmail.com>
2018-02-22 21:59:07 -0800

    Create user on first-time login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -8,6 +8,8 @@ from flask import (
     url_for
 )
 from app.util import reddit
+from app.db import db
+from app.db.models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -28,6 +30,14 @@ def auth_redirect():
     # Possible prawcore.exceptions.OAuthException here
 
     refresh_token = reddit.authorize(request.args.get('code'))
+    username = reddit.get_username(refresh_token)
+
     session['reddit_refresh_token'] = refresh_token
-    session['reddit_username'] = reddit.get_username(refresh_token)
+    session['reddit_username'] = username
+
+    if not User.query.filter_by(username=username).scalar():
+        user = User(username=username)
+        db.session.add(user)
+        db.session.commit()
+
     return redirect(url_for('raffles.create'))
