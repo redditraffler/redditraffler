4ff2de1966d5b43016df904efb243cf2c311ad32
Timothy Ng <timnjj@gmail.com>
2018-03-04 11:43:34 -0800

    Move dev and prod app init to runserver.py to prevent conflict with test app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,1 @@
-from flask.helpers import get_debug_flag
-from app.config import DevConfig, ProdConfig
-from app.factory import create_app
 
-cfg = DevConfig if get_debug_flag() else ProdConfig
-app = create_app(config_object=cfg)
--- runserver.py
+++ runserver.py
@@ -1,1 +1,10 @@
-from app import app
+# To start the server:
+# $ export FLASK_APP=runserver.py
+# $ flask run
+
+from flask.helpers import get_debug_flag
+from app.config import DevConfig, ProdConfig
+from app.factory import create_app
+
+cfg = DevConfig if get_debug_flag() else ProdConfig
+app = create_app(cfg)
