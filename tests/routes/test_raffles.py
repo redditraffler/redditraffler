67adf0fe274526126a89c4fb5a294cf4ba88724b
Timothy Ng <timnjj@gmail.com>
2018-03-04 12:08:18 -0800

    Stub get_submission with fake values

--- tests/routes/test_raffles.py
+++ tests/routes/test_raffles.py
@@ -16,9 +16,23 @@ def test_post_create_invalid_params(client):
 
 
 def test_post_create_valid_params(client, monkeypatch):
-    monkeypatch.setattr(reddit, 'get_submission', lambda sub_url: True)
+    monkeypatch.setattr(reddit, 'get_submission', _stub_submission)
+    monkeypatch.setattr(raffle, 'queue', _stub_raffle_job)
     res = client.post(url_for('raffles.create'), data=_valid_form_params())
     assert res.status_code == 200
+
+
+
+
+def _stub_submission(sub_url):
+    return {
+        'id': 'abc123',
+        'author': 'test_user',
+        'title': 'test_title',
+        'url': 'https://redd.it/abc123',
+        'subreddit': 'test',
+        'created_at_utc': 1520193497
+    }
 
 
 def _valid_form_params():
