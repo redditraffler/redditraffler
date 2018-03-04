01d57a50ccf03d50fa449f33134950115c78bacd
Timothy Ng <timnjj@gmail.com>
2018-03-03 21:33:08 -0800

    Set dev config if debug flag present

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+from flask.helpers import get_debug_flag
+from app.config import DevConfig, ProdConfig
 from app.factory import create_app
 
-app = create_app()
+cfg = DevConfig if get_debug_flag() else ProdConfig
+app = create_app(config_object=cfg)
