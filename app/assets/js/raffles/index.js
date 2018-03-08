85d73f7cc654187af4fa48b92aaa5beb1f58bcfd
Timothy Ng <timnjj@gmail.com>
2018-03-08 07:46:43 -0800

    Sort by created_at date by default

--- app/assets/js/raffles/index.js
+++ app/assets/js/raffles/index.js
@@ -1,5 +1,8 @@
 $(function() {
-    $("#raffles").DataTable({"lengthChange": false});
+    $("#raffles").DataTable({
+        "lengthChange": false,
+        "order": [[2, "desc"]]
+    });
 
     $("#raffles tbody tr").click(function() {
         var raffle_id = $(this).attr('data-raffle-id');
