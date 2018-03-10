902d39ace3556367d3b87727bdbcb2940d865b6e
Timothy Ng <timnjj@gmail.com>
2018-03-09 23:47:33 -0800

    Added click handler for each raffle

---
+++ app/assets/js/users/show.js
@@ -0,0 +1,6 @@
+$(function() {
+    $(".raffle-box").click(function() {
+        var raffle_id = $(this).attr('data-raffle-id');
+        window.location.replace($APP_ROOT + "raffles/" + raffle_id);
+    });
+});
