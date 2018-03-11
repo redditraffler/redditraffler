7e0cdd28cbb2a94730aeeeb695047b289b35aadd
Timothy Ng <timnjj@gmail.com>
2018-03-11 11:43:16 -0700

    Tweaked footer copy and style

--- app/assets/js/layouts/footer.js
+++ app/assets/js/layouts/footer.js
@@ -2,7 +2,12 @@ function updateClock() {
     $("#clock").html(moment().utc().format('MMMM Do YYYY, h:mm:ssa'));
 }
 
+function setCopyrightYear() {
+    $("#year").html(moment().utc().format('YYYY'));
+}
+
 $(function() {
+    setCopyrightYear();
     updateClock();
     setInterval(updateClock, 1000);
 });
