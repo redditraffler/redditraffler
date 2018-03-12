6bb588e6f709ac5971144a16fc9ddd89d0958650
Timothy Ng <timnjj@gmail.com>
2018-03-12 14:31:43 -0700

    Add TOS and privacy policy routes/views

--- app/routes/base.py
+++ app/routes/base.py
@@ -14,8 +14,18 @@ base = Blueprint('base', __name__)
 
 @base.route('/')
 def index():
-    return render_template(
-        'base/index.html',
-        title='Home',
-        reddit_login_url=reddit.get_auth_url()
-    )
+    return render_template('base/index.html',
+                           title='Home',
+                           reddit_login_url=reddit.get_auth_url())
+
+
+@base.route('/terms-of-service')
+def tos():
+    return render_template('base/terms-of-service.html',
+                           title='Terms of Service')
+
+
+@base.route('/privacy-policy')
+def privacy_policy():
+    return render_template('base/privacy-policy.html',
+                           title='Privacy Policy')
