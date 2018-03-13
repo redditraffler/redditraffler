6fb15530e4769362f40366477d8eacb553f2d1cc
Timothy Ng <5664347+timorthi@users.noreply.github.com>
2018-03-13 10:17:41 -0700

    Added asset minifying and bundling (#29)
    * Add local fontawesome js file
    
    * Add Flask-Assets
    
    * Use bundled files instead of individual files in base layout
    
    * Don't bundle assets in dev environment
    
    * Make asset imports single-line

--- app/extensions.py
+++ app/extensions.py
@@ -4,9 +4,11 @@ from flask_rq2 import RQ
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_wtf.csrf import CSRFProtect
+from flask_assets import Environment
 
 db = SQLAlchemy()
 migrate = Migrate(directory='app/db/migrations')
 rq = RQ()
 limiter = Limiter(key_func=get_remote_address, default_limits=['2 per second'])
 csrf = CSRFProtect()
+assets = Environment()
