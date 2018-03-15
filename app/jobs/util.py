e25be0f8132afbac5e8238364f8a78b95277f467
Timothy Ng <timnjj@gmail.com>
2018-03-15 07:59:43 -0700

    Add helper for setting job error

--- app/jobs/util.py
+++ app/jobs/util.py
@@ -1,3 +1,8 @@
 def update_job_status(job, status):
     job.meta['status'] = status
     job.save_meta()
+
+
+def set_job_error(job, is_error):
+    job.meta['error'] = is_error
+    job.save_meta()
