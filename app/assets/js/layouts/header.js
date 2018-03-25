c6976f01935b0947cef7f814a67f437220cd899c
Timothy Ng <timnjj@gmail.com>
2018-03-25 14:58:06 -0700

    Rewrite navbar js in jquery

--- app/assets/js/layouts/header.js
+++ app/assets/js/layouts/header.js
@@ -1,27 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
+$(function() {
+    // Show/hide functionality for navbar burger
+    var $navbarBurgers = $('.navbar-burger');
     if ($navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        $navbarBurgers.forEach(function ($el) {
-            $el.addEventListener('click', function () {
-
-                // Get the target from the "data-target" attribute
-                var target = $el.dataset.target;
-                var $target = document.getElementById(target);
-
-                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-                $el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-
+        $navbarBurgers.each(function() {
+            $(this).click(function() {
+                var $target = $('#'+$(this).attr('data-target'));
+                $(this).toggleClass('is-active');
+                $target.toggleClass('is-active');
             });
         });
     }
 
+    // Logout button click handler
     if ($("#logout").length > 0) {
         $("#logout").click(function() {
             $("#logout-form").submit();
