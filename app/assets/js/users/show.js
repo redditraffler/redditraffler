06ea5028b567ac5acaa91ecd1598f2933a7709e1
Timothy Ng <timnjj@gmail.com>
2018-05-21 13:04:34 -0700

    Add show more button in user raffle page

--- app/assets/js/users/show.js
+++ app/assets/js/users/show.js
@@ -1,6 +1,28 @@
+function initListControl() {
+    var INITIAL_ROW_COUNT = 10;
+    var rowCount = $(".raffle-box").length;
+    var visibleRowCount = 10;
+    if (rowCount > visibleRowCount) {
+        $("#table-control").show();
+    }
+
+    $(".raffle-box").hide();
+    $(".raffle-box:lt(" + visibleRowCount + ")").show();
+
+    $("#show-more").click(function() {
+        visibleRowCount = (visibleRowCount + 10 <= rowCount) ? visibleRowCount + 10 : rowCount;
+        $(".raffle-box:lt(" + visibleRowCount + ")").show();
+        if (visibleRowCount == rowCount) {
+            $("#show-more").hide();
+        }
+    });
+}
+
 $(function() {
     $(".raffle-box").click(function() {
         var raffle_id = $(this).attr('data-raffle-id');
         window.location.href = "/raffles/" + raffle_id;
     });
+
+    initListControl();
 });
