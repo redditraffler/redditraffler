4adbb83a78fda24e9a0cd0497aba4e46bdbcf3ff
Timothy Ng <timnjj@gmail.com>
2018-06-22 11:11:35 +0400

    Don't send user raffles to template

--- app/routes/users.py
+++ app/routes/users.py
@@ -13,14 +13,8 @@ users = Blueprint('users', __name__)
 @users.route('/<username>')
 def show(username):
     user = User.query.filter_by(username=username).one_or_none()
-
     if not user:
         abort(404)
-
-    raffles = Raffle.query.filter_by(user_id=user.id) \
-                          .order_by(desc('created_at'))
-
     return render_template('users/show.html',
                            title="/u/%s's Raffles" % user.username,
-                           user=user,
-                           raffles=raffles)
+                           user=user)
