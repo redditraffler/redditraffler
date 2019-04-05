6f7f667d5ff6a7beb8d47e2cf406be06ee51e434
Timothy Ng <timnjj@gmail.com>
2019-04-04 21:21:07 -0700

    Autoformat updates

--- app/db/migrations/versions/9cfa830d9291_migrate_ignored_user_to_column.py
+++ app/db/migrations/versions/9cfa830d9291_migrate_ignored_user_to_column.py
@@ -11,19 +11,19 @@ from sqlalchemy.dialects import postgresql
 from collections import defaultdict
 
 # revision identifiers, used by Alembic.
-revision = '9cfa830d9291'
-down_revision = 'dc43d91dc18a'
+revision = "9cfa830d9291"
+down_revision = "dc43d91dc18a"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     # Create new column for Raffles
-    op.add_column('raffle', sa.Column('ignored_users', sa.TEXT, nullable=True))
+    op.add_column("raffle", sa.Column("ignored_users", sa.TEXT, nullable=True))
 
     # Fetch all ignored_users
     db_conn = op.get_bind()
-    rows = db_conn.execute('SELECT raffle_id, username FROM ignored_user').fetchall()
+    rows = db_conn.execute("SELECT raffle_id, username FROM ignored_user").fetchall()
 
     # Group ignored users by raffle ID
     ignored_users_by_raffle_id = defaultdict(list)
@@ -32,14 +32,15 @@ def upgrade():
 
     # CSV-ify ignored users and set column value in Raffle
     for raffle_id, ignored_users in ignored_users_by_raffle_id.items():
-        ignored_users_str = ','.join(ignored_users)
+        ignored_users_str = ",".join(ignored_users)
         db_conn.execute(
             sa.text(
-                "UPDATE raffle SET ignored_users = :ignored_users_str WHERE raffle.id = :raffle_id"),
+                "UPDATE raffle SET ignored_users = :ignored_users_str WHERE raffle.id = :raffle_id"
+            ),
             ignored_users_str=ignored_users_str,
-            raffle_id=raffle_id
+            raffle_id=raffle_id,
         )
 
 
 def downgrade():
-    op.drop_column('raffle', 'ignored_users')
+    op.drop_column("raffle", "ignored_users")
