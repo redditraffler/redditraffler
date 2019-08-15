aaeb6a6b7f69cc30cd3987d9dce9061269360d40
Timothy Ng <timnjj@gmail.com>
2019-08-14 21:19:05 -0700

    Remove instance injector black magic

--- app/services/reddit_service.py
+++ app/services/reddit_service.py
@@ -1,32 +1,7 @@
 import praw
 import prawcore
-import functools
 
 from praw.models import Submission
-
-
-def with_reddit_instance(fnToWrap):
-    """ Injects a praw.Reddit instance, created using the RedditService instance's
-    settings, as a parameter to the wrapped function. If the refresh_token keyword
-    argument is present, an authenticated instance will be provided. """
-
-    @functools.wraps(fnToWrap)
-    def reddit_instance_injector(*args, **kwargs):
-        reddit_service_instance = args[0]
-        refresh_token = kwargs.get("refresh_token")
-
-        if refresh_token is not None:
-            return fnToWrap(
-                *args,
-                **kwargs,
-                reddit=praw.Reddit(
-                    **reddit_service_instance.settings, refresh_token=refresh_token
-                ),
-            )
-        else:
-            return fnToWrap(*args, **kwargs, reddit=reddit_service_instance._reddit)
-
-    return reddit_instance_injector
 
 
 class RedditService:
@@ -40,33 +15,32 @@ class RedditService:
         self.auth_scopes = app.config["REDDIT_AUTH_SCOPES"]
         self._reddit = praw.Reddit(**self.settings)
 
-    @with_reddit_instance
-    def get_oauth_url(self, reddit):
+    def get_oauth_url(self):
         """ Returns the URL that directs the user to Reddit's OAuth page. """
-        return reddit.auth.url(self.auth_scopes, "auth", "permanent")
+        return self._reddit.auth.url(self.auth_scopes, "auth", "permanent")
 
-    @with_reddit_instance
-    def authorize(self, reddit, code):
+    def authorize(self, code):
         """ Given a valid code from Reddit's OAuth redirect, returns a refresh token \
             for the user. """
-        return reddit.auth.authorize(code)
+        return self._reddit.auth.authorize(code)
 
-    @with_reddit_instance
-    def get_user_from_token(self, reddit, refresh_token=None):
+    def get_user_from_token(self, refresh_token):
         """ Returns the authorized Redditor. """
-        return reddit.user.me()
+        return praw.Reddit(**self.settings, refresh_token=refresh_token).user.me()
 
-    @with_reddit_instance
-    def get_submissions_for_user(self, reddit, refresh_token=None):
+    def get_submissions_for_user(self, refresh_token):
         """ Returns the submissions for the authorized Redditor. """
-        submissions = reddit.user.me().submissions.new()
+        submissions = (
+            praw.Reddit(**self.settings, refresh_token=refresh_token)
+            .user.me()
+            .submissions.new()
+        )
         return [_extract_submission_info(s) for s in submissions]
 
-    @with_reddit_instance
-    def get_submission_by_url(self, reddit, url):
+    def get_submission_by_url(self, url):
         try:
             submission_id = Submission.id_from_url(url)
-            submission = reddit.submission(submission_id)
+            submission = self._reddit.submission(submission_id)
             return _extract_submission_info(submission)
         except (prawcore.exceptions.NotFound, praw.exceptions.ClientException):
             return None
