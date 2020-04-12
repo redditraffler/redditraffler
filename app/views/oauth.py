cb66b302e7afd7f50728d77f0eac61696c2efbd0
Timothy Ng <timnjj@gmail.com>
2020-04-12 16:01:57 -0700

    Catch authorization failure only

--- app/views/oauth.py
+++ app/views/oauth.py
@@ -27,15 +27,14 @@ def authorize_oauth_code():
 
     try:
         auth_code = request.get_json().get("code")
-
         refresh_token = reddit_service.authorize(code=auth_code)
-        reddit_user = reddit_service.get_user_from_token(refresh_token)
-
-        user = User.find_or_create(reddit_user.username)
-        user.set_refresh_token(refresh_token)
-        jwt = user.get_jwt()
-
-        return jsonify(jwt=jwt)
     except:
-        return jsonify(error_message=FAILED_AUTH_CODE_AUTHORIZATION_MESSAGE), 500
+        return jsonify(error_message=FAILED_AUTH_CODE_AUTHORIZATION_MESSAGE), 422
 
+    reddit_user = reddit_service.get_user_from_token(refresh_token)
+
+    user = User.find_or_create(reddit_user.name)
+    user.set_refresh_token(refresh_token)
+    jwt = user.get_jwt()
+
+    return jsonify(jwt=jwt)
