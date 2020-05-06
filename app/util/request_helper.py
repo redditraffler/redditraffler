b469ec0bbfd5477a28017ce9ed0803f80a538e91
Timothy Ng <timnjj@gmail.com>
2020-05-05 21:28:07 -0700

    Add require_jwt

--- app/util/request_helper.py
+++ app/util/request_helper.py
@@ -1,6 +1,7 @@
 from functools import wraps
-from flask import session, abort, current_app
+from flask import session, abort, current_app, request, jsonify
 from app.util import jwt_helper
+from app.db.models.user import User
 
 
 def require_login(f):
@@ -15,6 +16,26 @@ def require_login(f):
         except:
             current_app.logger.exception("Failed to decode JWT", {"jwt": jwt})
             return abort(401)
+
+        return f(*args, **kwargs)
+
+    return decorated_fn
+
+
+def require_jwt(f):
+    @wraps(f)
+    def decorated_fn(*args, **kwargs):
+        auth_header = request.headers.get("Authorization", "")
+        if len(auth_header.split("Bearer ")) != 2:
+            return jsonify(error_message="Invalid or missing Authorization header"), 401
+
+        try:
+            jwt = auth_header.split("Bearer ")[1]
+            user = User.find_by_jwt(jwt)
+            kwargs["user"] = user
+        except:
+            current_app.logger.exception("Failed to fetch user by JWT", {"jwt": jwt})
+            return jsonify(error_message="Invalid Authorization header"), 401
 
         return f(*args, **kwargs)
 
