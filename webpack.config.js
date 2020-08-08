d3508d922f8acdc710e989e7a279f6a1dd560007
Timothy Ng <timnjj@gmail.com>
2020-08-08 16:41:03 -0700

    Add webpack dev flow

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const ManifestPlugin = require("webpack-manifest-plugin");
 module.exports = {
   mode: "development",
   entry: "./app/js/index.js",
+  devServer: {
+    contentBase: "./app/assets",
+    writeToDisk: true,
+  },
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "app/assets"),
