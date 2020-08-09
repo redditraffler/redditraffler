901c254639964ce61c4ef2177a9b3486d0bba94e
Timothy Ng <timnjj@gmail.com>
2020-08-08 17:50:14 -0700

    Split webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -3,14 +3,10 @@ const ManifestPlugin = require("webpack-manifest-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const targetPath = path.resolve(__dirname, "app/assets/js_build");
+module.exports.targetPath = targetPath;
 
 module.exports = {
-  mode: "development",
   entry: "./app/js/index.js",
-  devServer: {
-    contentBase: targetPath,
-    writeToDisk: true,
-  },
   output: {
     filename: "[name].[contenthash].js",
     path: targetPath,
---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: common.targetPath,
+    writeToDisk: true,
+  },
+});
