4a5525e43d3468641b031f79ff0f637a01f84bc3
Timothy Ng <timnjj@gmail.com>
2020-08-10 21:43:07 -0700

    Add babel and babel loader

--- webpack.common.js
+++ webpack.common.js
@@ -15,4 +15,18 @@ module.exports = {
     new CleanWebpackPlugin({ verbose: true, dry: false }),
     new ManifestPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
