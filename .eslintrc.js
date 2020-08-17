91da3a689f11087c6a8f2c5d07b4452279433cee
Timothy Ng <timnjj@gmail.com>
2020-08-16 22:53:29 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {},
+};
