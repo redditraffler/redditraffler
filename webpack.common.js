5a9ac36f65ce56a7b49efe3fff41e0e0584fae7c
Timothy Ng <timnjj@gmail.com>
2020-08-22 15:49:02 -0700

    Add babel/runtime

--- webpack.common.js
+++ webpack.common.js
@@ -56,6 +56,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
