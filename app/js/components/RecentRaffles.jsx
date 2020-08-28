ccd5ae34288773c5ebd59272bdbdf1afe6f93938
Timothy Ng <timnjj@gmail.com>
2020-08-27 21:19:26 -0700

    Scaffold RecentRaffles component

---
+++ app/js/components/RecentRaffles.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecentRaffles = () => {
+  return <div>hello recent raffles!</div>;
+};
+
+export default RecentRaffles;
--- app/js/pages/base/index.js
+++ app/js/pages/base/index.js
@@ -4,10 +4,16 @@ import ReactDOM from "react-dom";
 import "@assets/css/base/index.css";
 
 import RaffleMetrics from "@js/components/RaffleMetrics";
+import RecentRaffles from "@js/components/RecentRaffles";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
     <RaffleMetrics />,
     document.getElementById("raffle-stats-root")
+  );
+
+  ReactDOM.render(
+    <RecentRaffles />,
+    document.getElementById("recent-raffles-root")
   );
 });
