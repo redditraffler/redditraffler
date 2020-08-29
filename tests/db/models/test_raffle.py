81b164d4edca121f6305507c3bd00d87c46b20d2
Timothy Ng <timnjj@gmail.com>
2020-08-29 11:46:12 -0700

    Fix flaky test

--- tests/db/models/test_raffle.py
+++ tests/db/models/test_raffle.py
@@ -8,15 +8,15 @@ class TestRaffle:
     class TestGetVanityMetrics:
         def test_returns_correct_metrics(self, db_session):
             RaffleFactory.create_batch(2)
-            RaffleFactory.create_batch(4, subreddit="/r/topsubreddit")
-            RaffleFactory.create_batch(3, subreddit="/r/top2subreddit")
-            RaffleFactory.create_batch(2, subreddit="/r/top3subreddit")
+            RaffleFactory.create_batch(5, subreddit="/r/topsubreddit")
+            RaffleFactory.create_batch(4, subreddit="/r/top2subreddit")
+            RaffleFactory.create_batch(3, subreddit="/r/top3subreddit")
 
             result = Raffle.get_vanity_metrics()
 
             assert (
                 result["num_total_verified_raffles"]
-                == 2 + 4 + 3 + 2  # Based on counts above
+                == 2 + 5 + 4 + 3  # Based on counts above
             )
             num_winners_in_db = db_session.execute(
                 "select count(*) from winner"
@@ -27,9 +27,9 @@ class TestRaffle:
             assert result["num_total_winners"] == num_winners_in_db
             assert result["num_total_subreddits"] == num_distinct_subreddits_in_db
             assert result["top_recent_subreddits"] == [
-                {"subreddit": "/r/topsubreddit", "num_raffles": 4},
-                {"subreddit": "/r/top2subreddit", "num_raffles": 3},
-                {"subreddit": "/r/top3subreddit", "num_raffles": 2},
+                {"subreddit": "/r/topsubreddit", "num_raffles": 5},
+                {"subreddit": "/r/top2subreddit", "num_raffles": 4},
+                {"subreddit": "/r/top3subreddit", "num_raffles": 3},
             ]
 
     class TestGetRecentRaffles:
