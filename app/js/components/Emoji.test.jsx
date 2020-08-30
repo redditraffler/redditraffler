eb9e476852754c92db85992e312733c5a7cc90c2
Timothy Ng <timnjj@gmail.com>
2020-08-29 21:55:16 -0700

    Add test for Emoji component

---
+++ app/js/components/Emoji.test.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Emoji from "./Emoji";
+
+const testEmoji = "😈";
+
+test("renders the emoji with the correct props", async () => {
+  const testId = "emojiComponent";
+  const { getByTestId } = render(
+    <Emoji symbol={testEmoji} data-testid={testId} />
+  );
+
+  const component = await waitFor(() => getByTestId(testId));
+
+  expect(component).toHaveTextContent(testEmoji);
+  expect(component).toHaveAttribute("aria-label", "");
+  expect(component).toHaveAttribute("aria-hidden", "true");
+});
