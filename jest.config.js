1d6356b362aaaac1f9a5574550a907e88b7972ba
Timothy Ng <timnjj@gmail.com>
2020-09-01 22:13:32 -0700

    Match webpack aliases

--- jest.config.js
+++ jest.config.js
@@ -83,6 +83,9 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/app/js/__mocks__/fileMock.js",
     "\\.(css|less|sass|scss)$": "<rootDir>/app/js/__mocks__/styleMock.js",
+    // Below should match webpack.common.js aliases
+    "^@js(.*)$": "<rootDir>/app/js$1",
+    "^@assets(.*)$": "<rootDir>/app/assets$1",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
