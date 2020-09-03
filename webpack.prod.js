0a896bd29201609808a5bde703d63a39d2db6ad4
Timothy Ng <timnjj@gmail.com>
2020-09-02 19:52:48 -0700

    Add analyze_bundle command

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,15 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: process.env.BUNDLE_ANALYZER_ENABLED ? "server" : "disabled",
+      openAnalyzer: true,
+      analyzerHost: "localhost",
+      analyzerPort: 8888,
+    }),
+  ],
 });
