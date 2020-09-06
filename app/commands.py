90735e5e65967e6e8db28ffae895331b222380e0
Timothy Ng <timnjj@gmail.com>
2020-09-06 10:39:50 -0700

    Add command (script) to unescape submission titles

--- app/commands.py
+++ app/commands.py
@@ -1,4 +1,5 @@
 from flask.cli import with_appcontext
+from html import unescape
 import click
 
 from app.db.models.raffle import Raffle
@@ -33,3 +34,30 @@ def delete(raffle_id):
 def clear_cache():
     cache.clear()
     click.echo("[COMMAND] Cache cleared")
+
+
+@click.command(name="unescape_submission_titles")
+@with_appcontext
+def unescape_submission_titles():
+    try:
+        updated_raffles = []
+        for raffle in Raffle.query.all():
+            original_title = raffle.submission_title
+            updated_title = unescape(original_title)
+
+            if updated_title == original_title:
+                continue
+
+            raffle.submission_title = updated_title
+            updated_raffles.append(
+                {"original": original_title, "updated": updated_title}
+            )
+
+        db.session.commit()
+        click.echo("[COMMAND] Successfully unescaped raffle titles")
+        click.echo(updated_raffles)
+    except Exception as e:
+        db.session.rollback()
+        click.echo("[COMMAND] Error while trying to unescape submission titles")
+        click.echo(e)
+
