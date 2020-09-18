d934b8ab1601bf28e54e12af853acb525441dce5
Timothy Ng <timnjj@gmail.com>
2020-09-17 23:30:54 -0700

    Convert Emoji to ts

--- app/js/components/Emoji.tsx
+++ app/js/components/Emoji.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+type EmojiProps = Record<string, unknown> & {
+  label?: string;
+  symbol: string;
+};
+
 /**
  * Renders an Emoji.
  * Reference: https://medium.com/@seanmcp/%EF%B8%8F-how-to-use-emojis-in-react-d23bbf608bf7
  * @param {} props
  */
-const Emoji = (props) => (
+const Emoji: React.FC<EmojiProps> = (props) => (
   <span
     className="emoji"
     role="img"
