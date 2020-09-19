9bc5c6525ed8edf312a1dbb3907020e68d18d5af
Timothy Ng <timnjj@gmail.com>
2020-09-19 11:39:04 -0700

    Add yup schema and basic form structure

--- app/js/components/NewRaffle/NewRaffle.tsx
+++ app/js/components/NewRaffle/NewRaffle.tsx
@@ -1,7 +1,72 @@
 import React from "react";
+import PropTypes from "prop-types";
+import * as yup from "yup";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers";
+import Container from "react-bulma-components/lib/components/container";
+import Columns from "react-bulma-components/lib/components/columns";
+import Heading from "react-bulma-components/lib/components/heading";
 
-const NewRaffle: React.FC = () => {
-  return <div>hello new raffle</div>;
+const newRaffleFormSchema = yup
+  .object()
+  .shape({
+    submissionUrl: yup.string().required(),
+    winnerCount: yup.number().min(1).required(),
+    minAge: yup.number().min(0).required(),
+    usesCombinedKarma: yup.bool().required(),
+    minComment: yup.number().when("usesCombinedKarma", {
+      is: false,
+      then: yup.number().min(0).required(),
+      otherwise: yup.number(),
+    }),
+    minLink: yup.number().when("usesCombinedKarma", {
+      is: false,
+      then: yup.number().min(0).required(),
+      otherwise: yup.number(),
+    }),
+    minCombined: yup.number().when("usesCombinedKarma", {
+      is: true,
+      then: yup.number().min(0).required(),
+      otherwise: yup.number(),
+    }),
+    ignoredUsers: yup.array().of(yup.string()).required(),
+  })
+  .required();
+
+type NewRaffleFormSchema = yup.InferType<typeof newRaffleFormSchema>;
+
+interface NewRaffleProps {
+  csrfToken: string;
+}
+const NewRaffle: React.FC<NewRaffleProps> = ({ csrfToken }) => {
+  console.log({ csrfToken });
+
+  const formMethods = useForm({
+    resolver: yupResolver<NewRaffleFormSchema>(newRaffleFormSchema),
+  });
+
+  const onSubmit = async () => {
+    console.log("hello world");
+  };
+
+  return (
+    <FormProvider {...formMethods}>
+      <Container>
+        <form onSubmit={formMethods.handleSubmit(onSubmit)}>
+          <Columns>
+            <Columns.Column>
+              <Heading size={5}>First, choose your Reddit submission.</Heading>
+            </Columns.Column>
+          </Columns>
+        </form>
+      </Container>
+    </FormProvider>
+  );
+};
+
+NewRaffle.propTypes = {
+  csrfToken: PropTypes.string.isRequired,
 };
 
 export default NewRaffle;
