2fa52f8d41b1b5e58fb6f249515acd51539a6223
Timothy Ng <timnjj@gmail.com>
2020-09-21 20:33:16 -0700

    Handle loading and error case

--- app/js/components/NewRaffle/SubmissionPicker.tsx
+++ app/js/components/NewRaffle/SubmissionPicker.tsx
@@ -1,7 +1,82 @@
 import React from "react";
+import styled from "styled-components";
+import useAsync from "react-use/lib/useAsync";
+import Box from "react-bulma-components/lib/components/box";
+import Loader from "react-bulma-components/lib/components/loader";
+import Icon from "react-bulma-components/lib/components/icon";
+
+import { getFromApi, Endpoint } from "@js/api";
+import { GetUserSubmissionsAPIResponse } from "@js/types";
+import { colors } from "@js/theme";
+
+const BoxCenteredContent = styled(Box)`
+  display: flex;
+  justify-content: center;
+`;
+
+const ContentContainer = styled("div")`
+  display: flex;
+  justify-content: center;
+
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`;
 
 const SubmissionPicker: React.FC = () => {
-  return <div>hello submission picker</div>;
+  const { loading, error, value: userSubmissions } = useAsync(() =>
+    getFromApi<GetUserSubmissionsAPIResponse>(
+      Endpoint.getSubmissionsForCurrentUser
+    )
+  );
+
+  if (loading) {
+    return (
+      <BoxCenteredContent>
+        <div>
+          <ContentContainer>
+            <Loader
+              style={{
+                width: "5rem",
+                height: "5rem",
+                border: `3px solid ${colors.reddit}`,
+                borderTopColor: "transparent",
+                borderRightColor: "transparent",
+              }}
+            />
+          </ContentContainer>
+          <ContentContainer>
+            <p>Fetching your Reddit submissions...</p>
+          </ContentContainer>
+        </div>
+      </BoxCenteredContent>
+    );
+  }
+
+  if (error) {
+    return (
+      <BoxCenteredContent>
+        <div>
+          <ContentContainer>
+            <Icon size="large">
+              <span
+                className="fas fa-times fa-4x"
+                style={{ color: colors.errorText }}
+              />
+            </Icon>
+          </ContentContainer>
+          <ContentContainer>
+            <p>
+              An error occurred while trying to fetch your Reddit submissions.
+              Please try again.
+            </p>
+          </ContentContainer>
+        </div>
+      </BoxCenteredContent>
+    );
+  }
+
+  return <Box>hello world</Box>;
 };
 
 export default SubmissionPicker;
