d56cb6103a8d8590435b508226ae10f441b1dcc5
Timothy Ng <timnjj@gmail.com>
2020-09-30 21:01:15 -0700

    Add field label

--- app/js/components/NewRaffle/InputWithStaticButton.tsx
+++ app/js/components/NewRaffle/InputWithStaticButton.tsx
@@ -1,14 +1,17 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { useFormContext, Controller } from "react-hook-form";
-
 import {
   Field,
   Control,
   Input,
+  Label,
 } from "react-bulma-components/lib/components/form";
 
+import { colors } from "@js/theme";
+
 interface InputWithStaticButtonProps {
+  label?: string;
   staticText: string;
   inputType: "text" | "number";
   name: string;
@@ -19,6 +22,7 @@ interface InputWithStaticButtonProps {
  * The input is wrapped with the RHF Controller component.
  */
 const InputWithStaticButton: React.FC<InputWithStaticButtonProps> = ({
+  label,
   staticText,
   inputType,
   name,
@@ -26,18 +30,27 @@ const InputWithStaticButton: React.FC<InputWithStaticButtonProps> = ({
   const { control } = useFormContext();
 
   return (
-    <Field kind="addons">
-      <Control>
-        <Controller name={name} control={control} as={Input} type={inputType} />
-      </Control>
-      <Control>
-        <span className="button is-static">{staticText}</span>
-      </Control>
-    </Field>
+    <React.Fragment>
+      <Label style={{ color: colors.reddit }}>{label}</Label>
+      <Field kind="addons">
+        <Control>
+          <Controller
+            name={name}
+            control={control}
+            as={Input}
+            type={inputType}
+          />
+        </Control>
+        <Control>
+          <span className="button is-static">{staticText}</span>
+        </Control>
+      </Field>
+    </React.Fragment>
   );
 };
 
 InputWithStaticButton.propTypes = {
+  label: PropTypes.string,
   staticText: PropTypes.string.isRequired,
   inputType: PropTypes.oneOf(["text", "number"] as const).isRequired,
   name: PropTypes.string.isRequired,
