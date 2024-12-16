bfae4be3ea4885a3ceefcb2c91d7af8b833b68ee
Timothy Ng <5664347+timorthi@users.noreply.github.com>
2024-12-15 20:30:53 -0800

    Staging/Production hotfix 2024-12-15 (#591)
    * fix: use REDIS_URL
    
    * fix: no verify
    
    * fix: lowercase n?
    
    * fix: concat only if base url exists

--- app/config.py
+++ app/config.py
@@ -16,7 +16,11 @@ class BaseConfig:
     LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
     DEBUG_CONFIG = get_boolean_env("DEBUG_CONFIG")
 
-    RQ_REDIS_URL = os.getenv('REDIS_TEMPORARY_URL')
+    RQ_REDIS_URL = (
+        os.getenv("REDIS_URL") + "?ssl_cert_reqs=none"
+        if os.getenv("REDIS_URL")
+        else None
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -72,7 +76,9 @@ class DebugConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or ""
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1
+        )
     ROLLBAR_ENABLED = get_boolean_env("ROLLBAR_ENABLED")
 
 
@@ -81,7 +87,9 @@ class ProdConfig(BaseConfig):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or ""
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1
+        )
     ROLLBAR_ENABLED = True
 
 
